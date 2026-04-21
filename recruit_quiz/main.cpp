3f84baad07a9b41fe46264017cf766a7e67292d4
稲村　優太 <i.yuta6919@gmail.com>
2026-04-21 12:13:43 +0900

    問題の数値をランダムに

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct Question
+{
+	string q; //問題文
+	int a; //答え
+};
 
 
 int main() {
-	cout << "[リクルート試験対策クイズ]\n";
-	cout << "13x(-5)の答えは？\n";
-	int answer;
-	cin >> answer;
+	Question questions[] = {
+		{"13x(-5)", 13 * -5},
+		{"(-21)÷(-3)", -21 / -3},
+		{"7-(4+2)÷2", 7 - (4 + 2) / 2},
+	};
 
-	if(answer == 13*-5) {
-		cout << "正解！\n";
-	} else {
-		cout << "間違い！正解は" << "13*-5" << "\n";
+
+	cout << "[リクルート試験対策クイズ]\n";
+	for(const auto& e : questions) {
+		cout << e.q << "の答えは？\n";
+		int answer;
+		cin >> answer;
+
+		if(answer == e.a) {
+			cout << "正解！\n";
+		} else {
+			cout << "間違い！正解は" << e.a << "\n";
+		}
 	}
+	
 
 	return 0;
 }
