6c505f673d73d716ecdd52be097660bb1da9b3f3
稲村　優太 <i.yuta6919@gmail.com>
2026-04-28 10:45:03 +0900

    三角形の面積を追加とvector型に変更

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <random>
+#include <vector>
 using namespace std;
 
 struct Question
@@ -10,12 +12,42 @@ struct Question
 
 
 int main() {
-	Question questions[] = {
-		{"13x(-5)", 13 * -5},
-		{"(-21)€(-3)", -21 / -3},
-		{"7-(4+2)€2", 7 - (4 + 2) / 2},
-	};
+	vector<Question> questions(3);
 
+	random_device rd;
+	mt19937 gen(rd());
+
+	//Š|‚¯Z
+	int x = uniform_int_distribution<>(1, 30)(rand);
+	int y = uniform_int_distribution<>(1, 20)(rand);
+
+	questions[0].q = to_string(x) + "~" + to_string(y);
+	questions[0].a = x * y;
+
+
+	//OŠpŒ`‚Ì–ÊÏ
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 2;
+	questions.push_back({
+		"–ÊÏ" + to_string(x * y / 2) + "cm^2A’ê•Ó" + to_string(y) + "cm‚ÌOŠpŒ`‚Ì‚‚³‚ğ‹‚ß‚æ",
+		x })
+
+
+	//Š„‚èZ
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[1].q = to_string(x * y) + "€" + to_string(y);
+	questions[1].a = x * y;
+
+	//•¡G‚È®
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 10)(rand);
+	int z = uniform_int_distribution<>(1, 10)(rand);
+	int w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q = 
+		to_string(x) + "-(" + to_string(y) + "+" + to_string(z) + ")€" + 
+		to_string(w);
+	questions[2].a = x - (y + z) / w;
 
 	cout << "[ƒŠƒNƒ‹[ƒgŒ±‘ÎôƒNƒCƒY]\n";
 	for(const auto& e : questions) {
